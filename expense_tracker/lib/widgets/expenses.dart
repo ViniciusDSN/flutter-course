326ff4692a211aea9e7e2eb51c43f95b7e17ab66
Vinicius <vini.vdsn@gmail.com>
2025-05-28 18:33:10 -0300

    finished app
    added responsiveness

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -33,10 +33,17 @@ class _ExpensesState extends State<Expenses> {
       date: DateTime.now(),
       category: Category.leisure,
     ),
+    Expense(
+      title: 'Bus',
+      amount: 10,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
   ];
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
@@ -73,6 +80,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -94,9 +103,25 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
         children: [
-          Chart(expenses: _registeredExpenses), Expanded(child: mainContent),
+          Chart(
+            expenses: _registeredExpenses
+          ),
+          Expanded(
+            child: mainContent
+          ),
+        ],
+      ) : Row(
+        children: [
+          Expanded(
+            child: Chart(
+                expenses: _registeredExpenses
+              ),
+          ),
+          Expanded(
+            child: mainContent
+          ),
         ],
       ),
     );
